e4e8f228538f3c81bfccbf4b9d63909d66f27970
RDmitriy <vetrep@bk.ru>
2022-06-09 15:51:52 +0300

    defined date format (dd.mm.yyyy hh:mm)

---
+++ script.js
@@ -0,0 +1,42 @@
+const ENTRYPOINT = 'https://www.cbr-xml-daily.ru/daily_json.js';
+
+function loadData() {
+    return fetch(ENTRYPOINT).then((res) => res.json());
+}
+
+(async () => {
+    const data = await loadData();
+    console.log(data);
+
+    const { Timestamp, Valute: { USD } } = data;
+
+    const date = new Date(Timestamp);
+
+    const element = document.getElementById('date');
+
+    element.textContent = formatDate(date);
+})();
+
+function formatDate(date) {
+    //const day = date.getDate();
+    //const month = date.getMonth();
+    //const year = date.getFullYear();
+    //const hours = date.getHours();
+    //const minutes = date.getMinutes();
+
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth()).padStart(2, '0');
+    const year = date.getFullYear();
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+
+    return `${day}.${month[0] + (+month[1]+1)}.${year} ${hours}:${minutes}`;
+}
+
+//function padZero(value) {
+//    if (value < 10) {
+//        return `0${value}`;
+//    }
+//
+//    return value;
+//}
